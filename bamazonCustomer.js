113f471924c95142ee4c0f003dada9f31eecffd5
Jared <Jared@Littles-MBP.fios-router.home>
2018-02-02 16:19:40 -0500

    updating db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,7 @@ var productIdNumber = 0;
 var productName = "";
 var stockQuantity = 0;
 var newStockQuantity = 0;
+var productPrice = 0;
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -38,19 +39,45 @@ function queryItemName() {
     	questions();
 
     });
+    return;
 }
 
 function checkInventory() {
 	  
 	if (requestedQuantity > stockQuantity) {
-		console.log("no way");
+		console.log("Not enough inventory. We can special order it if you like");
 		return;
 	} else {
-		var query = connection.query("SET sql_safe_updates = 0 UPDATE products SET stock_quantity = newStockQuantity WHERE item_id = productIdNumber",
-			function(err, res) {
-				console.log("you've made changes")
-	});
+		updateProduct();
+	}
+	return;
 }
+
+function updateProduct() {
+	var newStockQuantity = stockQuantity - requestedQuantity;
+	console.log("Gathering your order...\n" + productName + " \n" + "Quantity: " + requestedQuantity);
+	var query = connection.query(
+    	"UPDATE products SET ? WHERE ?",
+    	[
+      		{
+        	stock_quantity: newStockQuantity
+      		},
+      		{
+        	item_id: productIdNumber
+      		}
+    	],
+    function(err, res) {
+      	console.log("Order ready!\n" );
+     	customerTotal();
+     	return;
+
+    }
+    
+)};
+function customerTotal() {
+	productPrice = productPrice * requestedQuantity;
+	console.log("Your total is: " + productPrice)
+	return;
 }
 
 function questions() {
@@ -65,15 +92,16 @@ function questions() {
   		}
 	]).then(function(answers) { 
 		productIdNumber = answers.item_id;
-		productName = inventoryName[productIdNumber];
-		stockQuantity = inventoryQuantityArr[answers.item_id]
+		productName = inventoryName[answers.item_id - 1];
+		stockQuantity = inventoryQuantityArr[answers.item_id -1];
+		productPrice = inventoryPrice[answers.item_id-1];
 			//var stuff = answers[answers.item_id].product_name;
 		
+		
 		requestedQuantity = answers.productQuantity;
-   		console.log(inventoryName[answers.item_id]);
    		checkInventory();
   	});
-
+return;
 }
 
 
